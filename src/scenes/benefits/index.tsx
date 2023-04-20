044f5e207073a20d6773134059f77802cee47259
Martins Greizis <grei0003@hz.nl>
2023-04-20 14:15:18 +0200

    benefits boxes

---
+++ src/scenes/benefits/index.tsx
@@ -0,0 +1,89 @@
+import HeaderText from "@/shared/HeaderText";
+import { BenefitType, SelectedPage } from "@/shared/types";
+import {
+  HomeModernIcon,
+  UserGroupIcon,
+  AcademicCapIcon,
+} from "@heroicons/react/24/solid";
+import { motion } from "framer-motion";
+import Benefit from "./Benefit";
+
+const benefits: Array<BenefitType> = [
+  {
+    icon: <HomeModernIcon className="h-6 w-6 " />,
+    title: "State of the Art Facilities",
+    description: "Occaecat proident pariatur do ex cupidatat elit amet cupidatat et adipisicing."
+  },
+  {
+    icon: <UserGroupIcon className="h-6 w-6 " />,
+    title: "100's of Diverse Classes",
+    description: "Ad mollit aliquip officia sit nisi ex nulla do esse ullamco esse ea."
+  },
+  {
+    icon: <AcademicCapIcon className="h-6 w-6 " />,
+    title: "Expert and Pro Trainers",
+    description: "Esse veniam sit labore in qui ut enim adipisicing laborum laborum aute cillum."
+  },
+]
+
+const container = {
+  hidden: {},
+  visible: {
+    transition: {
+      staggerChildren: 0.2,
+    }
+  }
+}
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const Benefits = ({ setSelectedPage }: Props) => {
+  return (
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+      <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
+      >
+        <motion.div 
+          className="md:my-5 md:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <HeaderText>MORE THAN JUST A GYM</HeaderText>
+          <p className="py-5 text-sm">
+            We provide world class fitness equipment, trainers and classes to
+            get you to your ultimate fitness goals with ease. We provide true
+            care into each and every member.
+          </p>
+        </motion.div>
+
+        <motion.div 
+          className="mt-5 items-center justify-between gap-8 md:flex"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          variants={container}
+        >
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
+        </motion.div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Benefits;
