bcdcb683a1c12728c555ddd334489eeb617c7b2d
Martins Greizis <grei0003@hz.nl>
2023-04-20 09:50:00 +0200

    mobile responsive navbar modal

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
-  const [menuToggled, setMenuToggled] = useState<boolean>(false);
+  const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
   const isAboveMediumScreen = useMediaQuery('(min-width: 1060px)');
 
   return (
@@ -40,7 +40,7 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
               </div>
             ) : (
               <button className="rounded-full bg-secondary-500 p-2"
-                onClick={() => setMenuToggled(!menuToggled)}
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
               >
                 <Bars3Icon className="w-6 h-6 text-white" />
               </button>
@@ -48,6 +48,26 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
           </div>
         </div>
       </div>
+
+      {/* Mobile menu modal */}
+      {!isAboveMediumScreen && isMenuToggled && (
+        <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+          {/* Close icon */}
+          <div className="flex justify-end p-12">
+            <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <XMarkIcon className="w-6 h-6 text-gray-400" />
+            </button>
+          </div>
+
+          {/* Menu */}
+          <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+            <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Benefits" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Our Classes" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+            <Link page="Contact Us" selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
